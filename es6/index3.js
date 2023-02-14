c929a4ed007ead61e1bcdcbdeaf68ee22395632d
bzz231456 <2314564128@qq.com>
2023-02-14 18:29:47 +0800

    原型对象和对象原型

--- es6/index3.js
+++ es6/index3.js
@@ -1,14 +1,15 @@
-function Star(name, age){
+function Star(name, age) {
     this.name = name;
     this.age = age;
 }
-Star.prototype.sing = function (){
+Star.prototype.sing = function () {
     console.log(this.name + "在唱歌。。。");
 }
 const p1 = new Star("lyf", 19);
 const p2 = new Star("shl", 20);
 p1.sing();
 p2.sing();
+// prototype原型对象，__proto__对象原型（叫法不一样，实际上指向同一个内存区域）
 console.log(p1.sing === p2.sing)//true,两个对象公用一个sing方法
 console.log(p1.__proto__ === p2.__proto__)
 console.log(p1.__proto__ === Star.prototype)
