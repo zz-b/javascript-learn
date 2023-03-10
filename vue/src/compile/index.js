4878de95a6dfcfd29f8050fe723ee4db372b7f30
bzz231456 <2314564128@qq.com>
2023-03-10 16:39:26 +0800

    数组排序

---
+++ vue/src/compile/index.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * */
+
+// 标签名 a-aaa
+const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`;  
+// 命名空间标签 aa:aa-xxx
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`;
+// 开始标签-捕获标签名
+const startTagOpen = new RegExp(`^<${qnameCapture}`); 
+// 结束标签-匹配标签结尾的 </div>
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);
+// 匹配属性
+const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/; 
+// 匹配标签结束的 >
+const startTagClose = /^\s*(\/?)>/;
+// 匹配 {{ }} 表达式
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g;
