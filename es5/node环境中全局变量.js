9cfa4075d5d5ac6ac47b31a005c519ba2a8c7dc1
bzz231456 <2314564128@qq.com>
2023-05-01 21:18:08 +0800

    fix

--- es5/node环境中全局变量.js
+++ es5/node环境中全局变量.js
@@ -6,6 +6,7 @@ console.log(global === globalThis)
 console.log(this === globalThis)
 // 判断当前环境是浏览器环境还是node环境
 if(typeof window !== 'undefined'){
+  // 不能写成typeof window !== undefined
   console.log("browser")
 }else{
   console.log("nodejs")
