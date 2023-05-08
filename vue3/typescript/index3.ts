a71f107f7e321abc86c0ace62fa96a631c28ba2e
bzz231456 <2314564128@qq.com>
2023-05-08 14:58:35 +0800

    fix

--- vue3/typescript/index3.ts
+++ vue3/typescript/index3.ts
@@ -4,10 +4,21 @@ function func2(){
 class Person{
   name: string
   age: number
+  msg: string
   constructor(name, age){
     this.name = name;
     this.age = age;
+    this.msg = this.name + this.age
   }
+}
+interface Iperson{
+  name:string,
+  age:number,
+  msg:string
+}
+function showMsg(person:Iperson) : string{
+  return person.age + person.name + person.msg;
 }
 const p = new Person("张三", 19)
 console.log(p)
+console.log(showMsg(p));
--- vue3/typescript/js/index3.js
+++ vue3/typescript/js/index3.js
@@ -1,10 +1,16 @@
 function func2() {
 }
-class Person {
-    constructor(name, age) {
+var Person = /** @class */ (function () {
+    function Person(name, age) {
         this.name = name;
         this.age = age;
+        this.msg = this.name + this.age;
     }
+    return Person;
+}());
+function showMsg(person) {
+    return person.age + person.name + person.msg;
 }
-const p = new Person("张三", 19);
+var p = new Person("张三", 19);
 console.log(p);
+console.log(showMsg(p));
